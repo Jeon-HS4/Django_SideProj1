b72a4fd3f92ad50ab1066be6b55e3bec086115f3
Jeon-HS4 <jin0004@tukorea.ac.kr>
2023-05-25 03:04:53 +0900

    중간저장

--- pybo/urls.py
+++ pybo/urls.py
@@ -11,7 +11,11 @@ urlpatterns = [
     
     path('diary/create/',
          views.diary_create, name='diary_create'),
-    path('diary/detail/<str:dates>/',
+    path('diary/detail/<str:dates><str:key>/',
          views.diary_detail, name='diary_detail'),
+    path('diary/modify/<str:dates><str:key>/',
+         views.diary_modify, name='diary_modify'),
+    path('diary/delete/<str:dates><str:key>/',
+         views.diary_delete, name='diary_delete'),
 
 ]
